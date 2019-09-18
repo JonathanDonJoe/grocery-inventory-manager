29d868615b47046d573b91fb76da8a8d2d5562bc
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-09-18 14:16:53 -0400

    added redux-promise middleware to resolve promises

--- redux-express/src/index.js
+++ redux-express/src/index.js
@@ -2,23 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reduxPromise from 'redux-promise'
 
 // 1. In order to use React with Redux, we need react-redux.
 import { Provider } from 'react-redux';
 
-// 2. Create a redux store, so that: Redux exists and the provider has the store
-import { createStore } from 'redux';
+// 2. Create a redux store, so that: Redux exists and the provider has the store.
+// 2.1 Redux comes with a function called applyMiddleware
+import { createStore, applyMiddleware } from 'redux';
+
 
 // 3. Reducers to populate the store.  Reducers go into the rootReducer.  Make individual reducers to go into the rootReducer
 import rootReducer from './reducers/rootReducer';
 
 // 4. Actually create the store by passing it the rootReducer
-const aPublix = createStore(rootReducer);
+// 4.1 to use middleware, we intercept createstore
+const theStoreWithSomeMiddleware = applyMiddleware(reduxPromise)(createStore)(rootReducer);
+
+// const aPublix = createStore(rootReducer);
 
 // Provider is the component that glues React and Redux.  We hand the provider to ReactDOM.render, and we hand the provider a prop of store
 
 ReactDOM.render(
-    <Provider store = {aPublix}>
+    <Provider store = {theStoreWithSomeMiddleware}>
         <App />
     </Provider>
     ,
